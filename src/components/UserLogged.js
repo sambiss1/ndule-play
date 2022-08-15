0987a63e12e2c28e249f3cf4b786a06a37e2369b
Sam <sambisselele@gmail.com>
2022-08-15 17:46:32 +0100

    Feat(Adding user logged container)

--- src/components/UserLogged.js
+++ src/components/UserLogged.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import { React, useContext } from 'react';
+
+import UserTest from "../images/user-test.png";
+
+import { FiChevronDown } from "react-icons/fi"
+
+
+import "../styles/userlogged.css"
 
 export const UserLogged = () => {
     return (
-        <div>UserLogged</div>
+        <div
+            className="userlooged--container"
+        >
+            <div
+                className="usertest__image--container"
+            >
+                <img
+                    src={UserTest}
+                    alt="user avatar"
+                    className="usertest__image--avatar"
+                />
+            </div>
+
+            <div>
+                <h2>Sam Bisselele</h2>
+            </div>
+            <div>
+                <FiChevronDown 
+                    className="chevron__dropdown"
+
+                />
+            </div>
+        </div>
     )
 }
 
