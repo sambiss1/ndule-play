4cf44995dfef9634acefd7dfd953f2bfb88f0c21
Sam <sambisselele@gmail.com>
2022-08-16 12:02:42 +0100

    Feat(Adding display style for album tab)

---
+++ src/components/AlbumItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AlbumItem = () => {
+    return (
+        <div
+            className="album__item--container"
+        >AlbumItem</div>
+    )
+}
+
+export default AlbumItem
--- src/components/AlbumsTabs.js
+++ src/components/AlbumsTabs.js
@@ -1,8 +1,33 @@
 import { React } from 'react';
 
+import AlbumItem from './AlbumItem';
+
+import "../styles/albumitem.css"
+
 export const AlbumsTabs = () => {
     return (
-        <div>AlbumsTabs</div>
+
+
+        <div
+            className="album__tabs--container"
+        >
+            <div><h3>Nouveautés</h3>
+
+            </div>
+            <div
+                className="album__tabs--panel"
+            >
+                <AlbumItem />
+                <AlbumItem />
+                <AlbumItem />
+                <AlbumItem />
+                <AlbumItem />
+            </div>
+
+
+        </div>
+
+
     )
 }
 
