5644fef522a7c2cc18b1f06a28ebb559c9d77882
Sam <sambisselele@gmail.com>
2022-08-24 17:52:38 +0100

    Refactor(Changing slider handler color to primary application color)

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,33 +5,34 @@ import SpotifyWebApi from "spotify-web-api-js";
 import { UserContext } from "../UserContext";
 
 export function Player() {
-    const spotifyApi = new SpotifyWebApi();
-    spotifyApi.setAccessToken(window.localStorage.getItem("token"));
-    const { anUri, play, setPlay } = useContext(UserContext);
-    return (
-        <div className="music__player--container">
-            <SpotifyPlayer
-                token={window.localStorage.getItem("token")}
-                uris={[anUri]}
-                styles={{
-                    activeColor: "#fff",
-                    bgColor: "#333",
-                    color: "#fff",
-                    loaderColor: "#fff",
-                    sliderColor: "#DE5000",
-                    trackArtistColor: "#ccc",
-                    trackNameColor: "#DE5000",
-                    
-                }}
-                autoPlay
-                callback={(state) => {
-                    if (!state.isPlaying) setPlay(false);
-                }}
-                play={play}
-                showSaveIcon
-            />
-        </div>
-    );
+  const spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(window.localStorage.getItem("token"));
+  const { anUri, play, setPlay } = useContext(UserContext);
+  return (
+    <div className="music__player--container">
+      <SpotifyPlayer
+        token={window.localStorage.getItem("token")}
+        uris={[anUri]}
+        styles={{
+          activeColor: "#fff",
+          bgColor: "#333",
+          color: "#fff",
+          loaderColor: "#fff",
+          sliderColor: "#DE5000",
+          sliderHandleColor: "#DE5000",
+          trackArtistColor: "#ccc",
+          trackNameColor: "#DE5000",
+
+        }}
+        autoPlay
+        callback={(state) => {
+          if (!state.isPlaying) setPlay(false);
+        }}
+        play={play}
+        showSaveIcon
+      />
+    </div>
+  );
 }
 
 export default Player;
